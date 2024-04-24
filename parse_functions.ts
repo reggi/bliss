1289f9c1767643a7fca486031ae521106f7546d1
Thomas Reggi <thomas@reggi.com>
2024-04-24 18:24:51 -0400

    Refactored the `parseFunctions` function to return an empty array.

--- parse_functions.ts
+++ parse_functions.ts
@@ -2,55 +2,10 @@ import ts from "npm:typescript";
 import { AstValues, Blissfile } from "./types.ts";
 import { createAstFromSource } from "./test_source.ts";
 
-interface ExportedFunctions {
-  [functionName: string]: ts.FunctionDeclaration | ts.ArrowFunction;
-}
-
 export function parseFunctions(ast: AstValues): Blissfile {
   const { sourceFile, typeChecker } = ast;
   if (!sourceFile) return [];
-
-  const exportedFunctions: ExportedFunctions = {};
-  ts.forEachChild(sourceFile, node => {
-    if (ts.isFunctionDeclaration(node) && node.name) {
-      const { name } = node.name;
-      exportedFunctions[name.text] = node;
-    } else if (ts.isVariableStatement(node)) {
-      const [declaration] = node.declarationList.declarations;
-      if (ts.isIdentifier(declaration.name)) {
-        const { name } = declaration.name;
-        const initializer = declaration.initializer;
-        if (ts.isArrowFunction(initializer) || ts.isFunctionExpression(initializer)) {
-          exportedFunctions[name.text] = initializer;
-        }
-      }
-    }
-  });
-
-  const blissfile: Blissfile = Object.keys(exportedFunctions).map(functionName => {
-    const func = exportedFunctions[functionName];
-    const isDefault = functionName === "default";
-    const name = isDefault ? undefined : functionName;
-    const parameters = func.parameters.map(param => {
-      const paramName = param.name.getText(sourceFile);
-      const type = typeChecker.getTypeAtLocation(param);
-      const typeString = typeChecker.typeToString(type);
-      return {
-        name: paramName,
-        required: !param.questionToken,
-        type: typeString,
-      };
-    });
-    return {
-      isDefault,
-      name,
-      parameters,
-    };
-  });
-
-  return blissfile;
-
-  return exportedFunctions;
+  return [];
 }
 
 const source = `export default (name: string) => name;`;
